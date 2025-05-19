31a6fac16d39bd3eec97bf25e597eb90bbaa92af
DEBASISH MISHRA <142622145+debasish9876@users.noreply.github.com>
2025-05-20 00:13:45 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
     { name: "Services", href: "#services" },
     { name: "About", href: "#about" },
     { name: "Contact", href: "#contact" },
-    { name: "Privacy & Policy", href: "/privacy-policy" },
+    { name: "Privacy & Policy", href: "privacy-policy" },
   ]
 
   const services = [
